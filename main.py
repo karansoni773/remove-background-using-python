4c12794d7e5f388557c58de2bdebf67e0acbb84d
karansoni7773 <156766722+karansoni773@users.noreply.github.com>
2024-08-06 18:53:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# pip install rembg pillow
+
+from rembg import remove
+from PIL import Image
+
+input_path = "in.png"
+output_path = "out.png"
+
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
